8f22f2d46eed07559bc6a924798e6437a678411d
saurabh singh <86177535+saurabhsngh5484@users.noreply.github.com>
2023-04-13 22:54:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function fnrc(str){
+	let ans=null;
+	let object={};
+	for (let index = 0; index < str.length; index++) {
+		let currentChar=str.charAt(index);
+		if(object[currentChar]==undefined){
+			object[currentChar]=1;	
+		}else{
+			object[currentChar]++;
+		}
+	}	
+	for (let index = 0; index < str.length; index++) {
+		let currentChar=str.charAt(index);
+		if(object[currentChar]==1){
+			ans=currentChar;
+			break;
+		}
+	}
+	return ans;
+}
+let str=prompt("Enter a string");
+alert(fnrc(str));
